588395354de1025ddc681918eee68ba996f173d1
Julia Pearson <julia.pearson@gmail.com>
2015-11-06 10:47:41 -0500

    user input working

--- main.js
+++ main.js
@@ -1,31 +1,46 @@
+  //declaring variables for all buttons
   var $red;
   var $blue;
   var $yellow;
   var $green;
+
+  var $highScore;
+
+  //If this is true, it is the computer's turn
   var computer=true;
 
-  //need an array that holds order
-  //will call clicked(on each element)
+  //need an array that holds computer's random order
+  //program will call clicked(on each element)
   var orderedClicks = [];
+  //this array will house userClicks
   var userClicks = [];
+
+  var beep = function(){
+    document.getElementById('beepTime').play();
+  }
 
   //blinks the button when clicked or called by computer clicks
   var clicked = function($element) {
-    if(!computer){
-      userClicks.push($element);
-    }
-    // console.log('got clicked');
-    // console.log('tried noFlash');
-    //closure
     var noFlash = function() {
       $element.removeClass('flash');
+      //check progress when we turn off flash
+      if(!computer && (userClicks.length === orderedClicks.length)){
+        compareClicks();
+      } 
     }
     //making invisable for 150 mili-seconds
     var flash = function() {
-      //console.log('tried flash');
+      //should be: 'computer? false'
+      console.log('computer? '+computer);
+      //adding element to user array if user is the one playing
+      if(!computer){
+        userClicks.push($element);
+        //should be 'at click 1,2,3,4,etc.'
+        console.log('at click '+userClicks.indexOf($element));
+      }
       $element.addClass('flash');
-      setTimeout(noFlash, 150); 
-      compareClicks();  
+      beep();
+      setTimeout(noFlash, 150);  
       // ! add sound effect  
     }
     return flash;
@@ -50,57 +65,89 @@
     }
   }
 
+  var userTurnAlert = function(){
+      alert('your turn!');
+      computer=false;
+  }
+
   //need func that reads orderedClicks array and calls clicked() on it
   var runOrderedClicks = function() {
-    //how do I space this out in time?
-    for (var i = 0; i < orderedClicks.length; i++) {
+    var i=0;
+    for (i = 0; i < orderedClicks.length; i++) {
+      //should be:  'computer? true, at index: 1,2,3,4,etc.'
+      console.log('computer? '+computer+", at index: "+i)
       setTimeout(clicked(orderedClicks[i]), i*700);
     };
-    setTimeout(function(){alert('you\'re turn!');},700*orderedClicks.length);
-    computer=false;
+    setTimeout(userTurnAlert,i*700);
   } 
 
   //need func that cuts out event listeners when player gets order wrong
   var lost = function() {
-    alert('you\'re turn!');
+    $red.off('click', clicked($red));
+    $blue.off('click', clicked($blue));
+    $yellow.off('click', clicked($yellow));
+    $green.off('click', clicked($green));
+    alert('you lost!');
   }
 
-  //need func that reads user clicks (out of generated array) and compares with orderedClicks to make sure is same
-  var generateUserSequence = function($element) {
-    userClicks.push($element);
-  }
+  // //need func that reads user clicks (out of generated array) and compares with orderedClicks to make sure is same
+  // var generateUserSequence = function($element) {
+  //   userClicks.push($element);
+  // }
 
   //need some kind of timeOut (after last item in userClicks is confirmed) that runs the func that calls clicked on every item in orderedClicks[]
   var compareClicks = function() {
-    if(userClicks.length != orderedClicks.length){
-      console.log('keep going');
-    }else{
+    // if(userClicks.length < orderedClicks.length){
+    //   console.log('keep going');
+    // }else{
+      var moveOn = true;
       for (var i = 0; i < orderedClicks.length; i++) {
         if(orderedClicks[i] !== userClicks[i]){
-          lost();
+          moveOn = false;
         }
       }
-      //clears user clicks
-      userClicks=[];
-      computer=true;
-      addToSequence();
-      runOrderedClicks();
-    }
+      if(moveOn) {
+        //clears user clicks
+        $highScore.text(userClicks.length);
+        console.log($highScore.val());
+        alert('you get to move on!');
+        userClicks=[];
+        computer=true;
+        addToSequence();
+        setTimeout(runOrderedClicks, 1000);
+      }else{
+        lost();
+      }
   }
 
- 
-
 $(document).ready(function() {
+  //assigning variables to HTML elements
   $red = $('#red');
   $blue = $('#blue');
   $yellow = $('#yellow');
   $green = $('#green'); 
-  $start = $('#start'); 
+  $start = $('#start');
+  $highScore = $('actual-score'); 
  
+  //Beginning game with item in array
   addToSequence();
-  $start.on('click', runOrderedClicks); 
+
+  //gets game started when start button pressed
+  $start.on('click', function(){
+    //warn user
+    alert('GET READY');
+    //start displaying the button's glow after 1 second
+    setTimeout(runOrderedClicks,1000);
+  }); 
+
+  //ready for user clicks!
   $red.on('click', clicked($red));
   $blue.on('click', clicked($blue));
   $yellow.on('click', clicked($yellow));
   $green.on('click', clicked($green));
 });
+
+
+
+
+
